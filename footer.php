115f83075038f7dd369ff17a9337834f810d9a0a
Eric Busch <ericbusch@gmail.com>
2024-04-20 19:10:31 -0400

    changed footer

--- footer.php
+++ footer.php
@@ -6,14 +6,14 @@
 
 <?php do_action( 'tailpress_content_after' ); ?>
 
-<footer id="colophon" class="site-footer pt-16 pb-8 md:pb-10" role="contentinfo">
+<footer id="colophon" class="site-footer pt-16 pb-8 md:pb-16 bg-primary-800" role="contentinfo">
 	<?php do_action( 'tailpress_footer' ); ?>
 
 	<div class="flex flex-col items-center">
-		<p class="font-zh text-4xl text-primary-700">山</p>
+		<p class="font-zh text-4xl text-primary-200">山</p>
 		<a href="/"
-		   class="uppercase tracking-[.2em] text-primary-900 text-lg mt-1.5"><?php echo get_bloginfo( 'name' ); ?></a>
-		<p class="text-primary-700 text-balance max-w-xl text-center mt-1.5"><?php echo get_bloginfo( 'description' ); ?></p>
+		   class="uppercase tracking-[.25em] text-primary-200 text-lg mt-1.5"><?php echo get_bloginfo( 'name' ); ?></a>
+		<p class="text-primary-400 text-balance max-w-xl text-center mt-1.5"><?php echo get_bloginfo( 'description' ); ?></p>
 	</div>
 </footer>
 
