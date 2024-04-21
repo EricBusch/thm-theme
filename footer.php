fe0ac5804080ca14ff47a0ca8cd323a3bd06146f
Eric Busch <ericbusch@gmail.com>
2024-04-20 22:50:07 -0400

    fixed footer

--- footer.php
+++ footer.php
@@ -9,10 +9,10 @@
 <footer id="colophon" class="site-footer pt-16 pb-8 md:pb-16 bg-primary-800" role="contentinfo">
 	<?php do_action( 'tailpress_footer' ); ?>
 
-	<div class="flex flex-col items-center">
+	<div class="flex flex-col items-center narrow-container">
 		<p class="font-zh text-4xl text-primary-200">山</p>
 		<a href="/"
-		   class="uppercase tracking-[.25em] text-primary-200 text-lg mt-1.5"><?php echo get_bloginfo( 'name' ); ?></a>
+		   class="uppercase tracking-[.25em] text-primary-200 text-sm sm:text-lg mt-1.5"><?php echo get_bloginfo( 'name' ); ?></a>
 		<p class="text-primary-400 text-balance max-w-xl text-center mt-1.5"><?php echo get_bloginfo( 'description' ); ?></p>
 	</div>
 </footer>
